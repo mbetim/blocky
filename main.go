6ee6151656467e289b170393c677e31ff89a4802
Matheus Betim <matheus.betim@synvia.com>
2024-02-19 22:24:10 -0300

    feat: install and set up viper

--- main.go
+++ main.go
@@ -1,11 +1,42 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
-*/
 package main
 
-import "github.com/mbetim/blocky/cmd"
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/mbetim/blocky/cmd"
+	"github.com/spf13/viper"
+)
 
 func main() {
+	StartConfig()
 	cmd.Execute()
+}
+
+func StartConfig() error {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	configBasePath := filepath.Join(homeDir, ".config", "blocky")
+	configFullPath := filepath.Join(configBasePath, "config.yaml")
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(configBasePath)
+
+	if _, err := os.Stat(configBasePath); os.IsNotExist(err) {
+		if err := os.Mkdir(configBasePath, os.ModePerm); err != nil {
+			return err
+		}
+	}
+
+	if _, err := os.Stat(configFullPath); os.IsNotExist(err) {
+		if _, err := os.Create(configFullPath); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
